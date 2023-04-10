f01501954f1261d137045acef732444410483793
Pei Yu <125331682@qq.com>
2023-04-10 10:03:04 +0800

    调整获取本机ip的方法实现

--- jplugin-cloud-rpc-server/src/main/java/net/jplugin/cloud/rpc/server/Plugin.java
+++ jplugin-cloud-rpc-server/src/main/java/net/jplugin/cloud/rpc/server/Plugin.java
@@ -6,12 +6,13 @@ import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 import net.jplugin.cloud.common.CloudPluginPriority;
 import net.jplugin.cloud.rpc.server.imp.RpcServerManager;
-import net.jplugin.common.kits.IpKit;
 import net.jplugin.core.config.api.CloudEnvironment;
 import net.jplugin.core.kernel.api.AbstractPlugin;
 import net.jplugin.core.kernel.api.PluginAnnotation;
 import net.jplugin.core.kernel.api.PluginEnvirement;
 import net.jplugin.core.service.api.RefService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -20,6 +21,8 @@ import java.util.Properties;
 
 @PluginAnnotation
 public class Plugin extends AbstractPlugin {
+    
+    private static final Logger log = LoggerFactory.getLogger(Plugin.class);
 
     @RefService
     RpcServerManager server;
@@ -62,7 +65,11 @@ public class Plugin extends AbstractPlugin {
     }
     
     private String getIp() throws UnknownHostException {
-        return IpKit.getLocalIp();
-//        return InetAddress.getLocalHost().getHostAddress();
+//        return IpKit.getLocalIp();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        if (log.isInfoEnabled()) {
+            log.info("获取的本机ip:" + ip);
+        }
+        return ip;
     }
 }
