b0451f9649b1a7bd6787c02c637b727be7226867
Pei Yu <125331682@qq.com>
2023-05-17 16:20:29 +0800

    修复启动时，热更新模块报错的问题

--- jplugin-cloud-config/src/main/java/net/jplugin/cloud/config/ConfigChangeListener.java
+++ jplugin-cloud-config/src/main/java/net/jplugin/cloud/config/ConfigChangeListener.java
@@ -3,6 +3,7 @@ package net.jplugin.cloud.config;
 import com.alibaba.nacos.api.config.listener.Listener;
 import com.alibaba.nacos.shaded.com.google.common.collect.Lists;
 import net.jplugin.core.config.impl.ConfigureChangeManager;
+import net.jplugin.core.kernel.api.PluginEnvirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,6 +32,10 @@ public final class ConfigChangeListener implements Listener {
             log.debug("变更后值为:{}", configInfo);
         }
         NacosConfigProvidor.me().updateConfig(this.dataId, this.groupid, configInfo);
-        ConfigureChangeManager.instance.fireConfigChange(Lists.newArrayList(this.groupid));
+        //由于nacos刚启动时也会触发这个监听，框架可能还没启动完成，所以调整为只有整个工程启动完成后，Nacos触发监听事件，才会专递给热更新模块
+        if (PluginEnvirement.INSTANCE.getStateLevel() == PluginEnvirement.STAT_LEVEL_WORKING) {
+            ConfigureChangeManager.instance.fireConfigChange(Lists.newArrayList(this.groupid));
+        }
+        
     }
 }
